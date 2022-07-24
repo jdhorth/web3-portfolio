d550a20f7c46a4e57f512e8f13ac4d53392dd4ed
Justin Horth <jdhorth@gmail.com>
2022-07-24 17:03:29 -0400

    about info

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = () => {
         <section className='about'>
             <h2>About Me</h2>
             <p>
-                Welcome and thanks for surfing by. I am a born again web3 developer. I dove down the NFT and crypto rabbit hole and along the way built Non-Fungi.com. Some tutorial projects I've followed by awesome channels including EatTheBlocks, Dapp University, The Net Ninja, Traversy Media, Kevin Powell, Colby Fayock, freecodecamp and many more. I really appreciate all the educational and inspirational channels available! I'm excited to learn new web2 and web3 skills. I enjoy HTML, CSS, Javascript, frameworks, React, Nextjs, Gatsby, some Vue3 and am also interested in blockchain developments and NFTs. This is a new web3 porfolio project.
+                Welcome and thanks for surfing by. I am aspiring freelance musician and web2 / web3 developer / service tech. I dove down the NFT and crypto rabbit hole and along the way built Non-Fungi.com. Some tutorial projects I've followed by awesome channels including EatTheBlocks, Dapp University, The Net Ninja, Traversy Media, Kevin Powell, Colby Fayock, freecodecamp and many more. I really appreciate all the educational and inspirational channels available! I'm excited to learn new web2 and web3 skills. I enjoy HTML, CSS, Javascript, frameworks, React, Nextjs, Gatsby, some Vue3 and am also interested in blockchain developments and NFTs. This is a new web3 porfolio project.
             </p>
         </section>
     );
