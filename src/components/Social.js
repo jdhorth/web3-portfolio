e08816498376fbb93be63fe25ea6492838624dd8
Justin Horth <jdhorth@gmail.com>
2022-07-24 17:08:40 -0400

    info updates

--- src/components/Social.js
+++ src/components/Social.js
@@ -5,8 +5,9 @@ const Social = () => {
 
 
             <a href="https://twitter.com/jdhorth" className="button">Twitter</a>
-            <a href="https://www.linkedin.com/" className="button">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/justin-horth-90521110/" className="button">LinkedIn</a>
             <a href="https://github.com/jdhorth" className="button">GitHub</a>
+            <a href="https://justinhorth.com" className="button">web2 Portfolio</a>
         </div>
     );
 }
