b52388143e0818716113e805b99a77af43900998
Justin Horth <jdhorth@gmail.com>
2022-09-10 02:15:39 -0400

    spelling

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <section className='about'>
             <h2>About Me</h2>
             <p>
-                I am aspiring freelance musician and web2 / web3 developer / service tech / creative. I dove down the NFT and crypto rabbit hole and built and blogged on Non-Fungi.com. Some tutorial projects I've followed include EatTheBlocks, Dapp University, The Net Ninja, Traversy Media, Kevin Powell, Colby Fayock, Ania Kubow, freecodecamp and many more. I really appreciate all the educational and inspirational channels, blogs, videos and content available! I'm excited to learn new web2 and web3 skills. I enjoy HTML, CSS, Javascript, frameworks, have done tutorials in React, Nextjs, Gatsby, some Vue3, Svelte, Solid and so on, and am also interested in blockchain developments and NFTs. This is a new web3 porfolio project.
+                I am aspiring freelance musician and web2 / web3 developer / service tech / creative. I dove down the NFT and crypto rabbit hole and built and blogged on Non-Fungi.com. Some tutorial projects I've followed include EatTheBlocks, Dapp University, The Net Ninja, Traversy Media, Kevin Powell, Colby Fayock, Ania Kubow, freecodecamp and many more. I really appreciate all the educational and inspirational channels, blogs, videos and content available! I'm excited to learn new web2 and web3 skills. I enjoy HTML, CSS, Javascript, frameworks, have done tutorials in React, Nextjs, Gatsby, some Vue3, Svelte, Solid and so on, and am also interested in blockchain developments and NFTs. This is a new web3 portfolio project.
             </p>
         </section>
     );
